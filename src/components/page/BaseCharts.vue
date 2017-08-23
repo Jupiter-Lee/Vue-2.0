a359a0474d9966a62b41067035af3b6464bb06e2
Jupiter-Lee <450663973@qq.com>
2017-08-23 17:48:53 +0800

    可视化图表自适应性未完成

--- src/components/page/BaseCharts.vue
+++ src/components/page/BaseCharts.vue
@@ -7,35 +7,93 @@
                 <el-breadcrumb-item>基础图表</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
+        <el-row :gutter="10">
+            <el-col :xs="4" :sm="6" :md="8" :lg="12">
+                <div class="grid-content bg-purple-light status" style="height: 400px">
+                    <div class="header">
+                        <i class="icon-star"></i>
+                        <h3>集中器总览</h3>
+                    </div>
+                    <div class="stats">
+                        <div class="stat stat2 stat-link">
+                            <span class="stat-value" id="crNum">26970</span>
+                            集中器总数
+                        </div>
 
-        <div class="row">
-            <div class="col-md-6" style="background-color: #dedef8; box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+                        <div class="stat stat2 stat-link">
+                            <span class="stat-value" id="onNumber">6084</span>
+                            在线数
+                        </div>
+                    </div>
+                    <div class="footer">
+                        <div class="stats">
+                            <div class="stat stat3 stat-link">
+                                <span class="stat-value" id="linkRate">22%</span>
+                                连通率
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="12">
+                <div class="grid-content bg-purple status" style="height: 400px">
+                   <div class="header">
+                        <i class="icon-star"></i>
+                        <h3>表计总览</h3>
+                    </div>
+                    <div class="stats">
+                        <div class="stat stat2 stat-link">
+                            <span class="stat-value" id="crNum">26970</span>
+                            表计总数
+                        </div>
 
-                <div id="concentrator" class="concentrator" style="width: 600px;height:400px;"></div>
+                        <div class="stat stat2 stat-link">
+                            <span class="stat-value" id="onNumber">6084</span>
+                            在线数
+                        </div>
+                    </div>
+                    <div class="footer">
+                        <div class="stats">
+                            <div class="stat stat3 stat-link">
+                                <span class="stat-value" id="linkRate">22%</span>
+                                连通率
+                            </div>
+                        </div>
+                    </div>                    
+                </div>
+            </el-col>
+        </el-row>
 
-            </div>
-            <div class="col-md-6" style="background-color: #dedef8; box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+<!--         
+                    <div id="meter" class="meter" style="width: 600px;height:400px;"></div>
 
-                <div id="meter" class="meter" style="width: 600px;height:400px;"></div>
+                     -->
+        <el-row :gutter="10">
+            <el-col :xs="4" :sm="6" :md="8" :lg="12">
+                <div class="grid-content bg-purple-light">
+                    <div class="columnChart">
+                        <div class="main"></div>
+                    </div>
+                </div>
+            </el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="12">
+                <div class="grid-content bg-purple">
+                    4
+                </div>
+            </el-col>
+        </el-row>
 
-            </div>
-            <!-- 
-                <div class="crData"></div>
-                <div class="meterData"></div>
-     -->
-
-        </div>
     </div>
 </template>
 
 <script>
-import Schart from 'vue-schart';
 import echarts from 'echarts'
 export default {
     components: {
-        Schart
     },
     data: () => ({
+        width: 600,
+        height: container.clientHeight
     }),
     mounted() {
         var concentrator = echarts.init(document.getElementById('concentrator'));
@@ -117,6 +175,57 @@ export default {
 </script>
 
 <style scoped>
+.status {
+    margin-bottom: 20px;
+}
+
+.row {
+    height: 400px;
+}
+
+.header h3 {
+    background: #4fc08d;
+    color: #fff;
+    padding: 10px 15px;
+    margin-bottom: 20px;
+}
+.footer {
+    padding-top: 80px;
+}
+
+.stats {
+    width: 100%;
+    display: table;
+    padding: 0 0 0 10px;
+    margin-top: .5em;
+    margin-bottom: 1.9em;
+}
+.stats .stat {
+    display: table-cell;
+    width: 40%;
+    vertical-align: top;
+    font-size: 11px;
+    font-weight: bold;
+    color: #999;
+}
+.stats .stat.stat2 {
+    width: 50%;
+    text-align: center;
+}
+.stat.stat3 {
+    width: 100%;
+    text-align: center;
+}
+
+.stat-value {
+    display: block;
+    margin-top: .55em;
+    margin-bottom: .55em;
+    font-size: 40px;
+    font-weight: bold;
+    letter-spacing: -2px;
+    color: #444;
+}
 #concentrator {
     background: aliceblue;
 }
@@ -150,5 +259,26 @@ export default {
     height: 400px;
     float: right;
     border: solid #aaaaaa 1px;
+}
+
+.el-col {
+    border-radius: 4px;
+}
+
+.bg-purple-dark {
+    background: #99a9bf;
+}
+
+.bg-purple {
+    background: #d3dce6;
+}
+
+.bg-purple-light {
+    background: #e5e9f2;
+}
+
+.grid-content {
+    border-radius: 4px;
+    min-height: 36px;
 }
 </style>
